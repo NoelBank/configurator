af6b0cd765fc82b70514cb326c8e8ae42d140aaf
nbank <bank@neoskop.de>
2022-11-15 14:39:10 +0100

    fixes

--- hooks/useProzess/useProzess.test.ts
+++ hooks/useProzess/useProzess.test.ts
@@ -3,16 +3,34 @@ import { renderHook, act } from "@testing-library/react";
 import POWER_GAS_CONFIG from "../../configs/PowerGas.prozess";
 import useProzess from "./useProzess";
 
-describe("useProzess", () => {
-  test("should create a prozess", () => {
-    const { result } = renderHook(() =>
-      useProzess({
-        config: POWER_GAS_CONFIG,
-        initialState: {},
-        name: "power-gas-configurator",
-      })
-    );
+describe("prozess", () => {
+  describe("when created", () => {
+    it("should throw error if no prozess is started", () => {
+      const { result } = renderHook(() =>
+        useProzess({
+          config: POWER_GAS_CONFIG,
+          initialState: {},
+          name: "power-gas-configurator",
+        })
+      );
 
-    expect(result.current).toBeDefined();
+      expect(result.current.currentStep).toBeUndefined();
+    });
+
+    it("should start the prozess", () => {
+      const { result } = renderHook(() =>
+        useProzess({
+          config: POWER_GAS_CONFIG,
+          initialState: {},
+          name: "power-gas-configurator",
+        })
+      );
+
+      act(() => {
+        result.current.start();
+      });
+
+      expect(result.current.currentStep).toBeDefined();
+    });
   });
 });
--- hooks/useProzess/useProzess.ts
+++ hooks/useProzess/useProzess.ts
@@ -2,25 +2,16 @@ import { UseProzessOptions } from "./types";
 import { atom, useAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import { Prozess } from "../../libs/prozess/prozess";
+import React from "react";
 
 const useProzess = <State>({
   config,
   initialState,
   name,
 }: UseProzessOptions<State>) => {
-  const prozessAtom = atomWithStorage(name, initialState);
-
-  // const [data, setData] = useAtom(prozessAtom);
-
   const prozess = new Prozess(config, initialState);
 
-  const startProzess = () => {
-    prozess.start();
-  };
-
-  startProzess();
-
-  return {};
+  return prozess;
 };
 
 export default useProzess;
